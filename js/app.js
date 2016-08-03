8d3d4ceb2c4db29515848a67c660c40e95894395
gregoryquick <gregory@quicks.me>
2016-08-03 12:29:49 -0700

    More pages
    -added more pages
    -JS can now handle non-home menu items

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@ var main = function() {
   $('.menuItem').click(function(){
     if($(this).text() === 'Home') {
       window.location.href = 'index.html';
+    }else{
+      window.location.href = 'pages/' + $(this).text() +'.html';
     };
   });
 };
