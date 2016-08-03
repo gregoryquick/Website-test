2fdcfd58ef7a5364e34d23eaf076ad2480e88c16
gregoryquick <gregory@quicks.me>
2016-08-02 19:13:55 -0700

    Prettying
    -Removed comment clutter
    -Made menu refresh page
    -Changed dimensions and colors in css sheet

--- js/app.js
+++ js/app.js
@@ -1,35 +1,5 @@
-var holder = false;
-var toggle = function() {
-  if(holder){
-    holder = false;
-  } else {
-    holder = true;
-  }
-};
-
 var main = function() {
-  // alert("The page has loaded");
-  // $('.spambutton').animate({left: "285px"}, 200);
-  $('.spambutton').click(function(){
-    alert("You clicked an image!");
-    // $('.spambutton').animate({left: "2085px"}, 200);
-  });
-
-  $('.lpfgt1').click(function(){
-    $(this).toggleClass('lpfgt2');
-  });
-
-  $(document).keypress(function(event){
-    if(event.which <= 122 && event.which >= 65){
-      alert("You pressed letter key!\n"+ event.which);
-    }else{
-    alert("You pressed a button!\n"+ event.which);
-    }
-  });
+  
 };
-
-// while(holder){
-//   alert("Spam!")
-// }
 
 $(document).ready(main);
